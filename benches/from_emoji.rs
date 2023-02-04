e9a7c0a81901f1980d67ab3fa3e1a3a7287db6ca
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-02-04 23:01:48 +0100

    added some benchmarks for from_emoji

---
+++ benches/from_emoji.rs
@@ -0,0 +1,12 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use twemoji_assets::svg::SvgTwemojiAsset;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("from_emoji(BUCKET)", |b| b.iter(|| SvgTwemojiAsset::from_emoji(black_box("🪣"))));
+    c.bench_function("from_emoji(DUCK)", |b| b.iter(|| SvgTwemojiAsset::from_emoji(black_box("🦆"))));
+    c.bench_function("from_emoji(WARNING)", |b| b.iter(|| SvgTwemojiAsset::from_emoji(black_box("⚠️"))));
+    c.bench_function("from_emoji(HEART ON FIRE)", |b| b.iter(|| SvgTwemojiAsset::from_emoji(black_box("❤️‍🔥"))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
