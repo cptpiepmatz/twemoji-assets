c834cf4e4ee958e8a9c418093bb2653b953da21c
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-02-12 09:49:54 +0100

    fixed from_name, had incorrect input

--- benches/from_name.rs
+++ benches/from_name.rs
@@ -2,10 +2,10 @@ use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use twemoji_assets::svg::SvgTwemojiAsset;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("from_name(BUCKET)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("🪣"))));
-    c.bench_function("from_name(DUCK)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("🦆"))));
-    c.bench_function("from_name(WARNING)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("⚠️"))));
-    c.bench_function("from_name(HEART ON FIRE)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("❤️‍🔥"))));
+    c.bench_function("from_name(BUCKET)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("bucket"))));
+    c.bench_function("from_name(DUCK)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("duck"))));
+    c.bench_function("from_name(WARNING)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("warning"))));
+    c.bench_function("from_name(HEART ON FIRE)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("heart_on_fire"))));
 
     c.bench_function("from_name(no emoji)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box("not an emoji"))));
     c.bench_function("from_name(empty string)", |b| b.iter(|| SvgTwemojiAsset::from_name(black_box(""))));
